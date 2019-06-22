76850f2bfc08dfc34dec0faa30730c9e21dd12e4
Didier Cruz <didier.cruz98@gmail.com>
2019-06-22 03:47:08 -0600

    recursos extra

---
+++ parabellum/src/main/java/com/parabellum/springboot/web/app/models/service/UsuarioServiceImpl.java
@@ -0,0 +1,46 @@
+package com.parabellum.springboot.web.app.models.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.parabellum.springboot.web.app.models.dao.IUsuarioDao;
+import com.parabellum.springboot.web.app.models.entity.Usuario;
+
+@Service
+public class UsuarioServiceImpl implements IUsuarioService {
+	
+	@Autowired
+	private IUsuarioDao usuarioDao;
+	
+	@Transactional(readOnly = true)
+	public List<Usuario> findAll() {
+		
+		return usuarioDao.findAll();
+		
+	}
+	
+	@Transactional
+	public void save(Usuario usuario) {
+		
+		usuarioDao.save(usuario);
+		
+	}
+	
+	@Transactional(readOnly = true)
+	public Usuario findOne(Long id) {
+		
+		return usuarioDao.findOne(id);
+		
+	}
+	
+	@Transactional
+	public void delete(Long id) {
+		
+		usuarioDao.delete(id);
+		
+	}
+
+}
