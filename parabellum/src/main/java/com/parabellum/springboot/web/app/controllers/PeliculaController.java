88ed8dd75952a3825acc11fe5f83b80374191a51
Didier Cruz <didier.cruz98@gmail.com>
2019-06-25 11:15:45 -0600

    Integración de Pantalla listar Peliculas

--- parabellum/src/main/java/com/parabellum/springboot/web/app/controllers/PeliculaController.java
+++ parabellum/src/main/java/com/parabellum/springboot/web/app/controllers/PeliculaController.java
@@ -2,15 +2,22 @@ package com.parabellum.springboot.web.app.controllers;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.parabellum.springboot.web.app.models.entity.Pelicula;
+import com.parabellum.springboot.web.app.models.service.IPeliculaService;
 
 @Controller
 @RequestMapping("/admin")
 public class PeliculaController {
+	
+	
+	@Autowired
+	IPeliculaService peliculaService;
 
 	@GetMapping("/movie-form")
 	public String crear(Map<String, Object> model) {
@@ -18,6 +25,13 @@ public class PeliculaController {
 		model.put("pelicula", pelicula);
 		model.put("titulo","Formulario de Pelicula");
 		return "forms/movie-form";
+	}
+	
+	@GetMapping("/peliculas")
+	public String movieList(Model model) {
+		model.addAttribute("titulo", "Listado de Peliculas");
+		model.addAttribute("peliculas", peliculaService.findAll());
+		return "tables/movie-table";
 	}
 	
 }
