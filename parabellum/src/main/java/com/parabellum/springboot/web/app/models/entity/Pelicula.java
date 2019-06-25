c0463c4e25fb6f8da1fba8fb5244b3977d8bc25f
cjburgos21 <37281082+cjburgos21@users.noreply.github.com>
2019-06-25 10:48:48 -0600

    Entity Pelicula

---
+++ parabellum/src/main/java/com/parabellum/springboot/web/app/models/entity/Pelicula.java
@@ -0,0 +1,118 @@
+package com.parabellum.springboot.web.app.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "peliculas")
+public class Pelicula implements Serializable{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_pelicula")
+	private Long idPelicula;
+	
+	@NotEmpty
+	private String nombre;
+	
+	@NotEmpty
+	private String duracion;
+	
+	@NotNull
+	private boolean estreno;
+	
+	@NotEmpty 
+	private String genero;
+	
+	@NotEmpty
+	private String formato;
+	
+	@NotEmpty
+	private String idioma;
+	
+	@NotNull
+	private boolean estado;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public Long getIdPelicula() {
+		return idPelicula;
+	}
+
+	public void setIdPelicula(Long idPelicula) {
+		this.idPelicula = idPelicula;
+		
+	}
+		
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void setDuracion(String duracion) {
+		this.duracion = duracion;
+	}
+	
+	
+	public String getDuracion() {
+		return duracion;
+	}
+	
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+	
+	public boolean isEstado() {
+		return estado;
+	}
+	
+	public void setEstreno(boolean estreno) {
+		this.estreno = estreno;
+	}
+	
+	public boolean isEstreno() {
+		return estreno;
+	}
+	
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
+	public String getGenero() {
+		return genero;
+	}
+	
+	public void serFormato(String formato) {
+		this.formato = formato;
+	}
+	
+	public String getFormato() {
+		return formato;
+	}
+	
+	public void setIdioma(String idioma) {
+		this.idioma = idioma;
+	}
+	
+	public String getIdioma() {
+		return idioma;
+	}
+	
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+}
