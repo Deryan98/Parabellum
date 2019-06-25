0652e6718272073c34a17848c37eaf3ad6bb2e46
Cmagana6 <00061016@uca.edu.sv>
2019-06-24 23:18:05 -0600

    OneToMany en peliculas

--- parabellum/src/main/java/com/parabellum/springboot/web/app/models/entity/Pelicula.java
+++ parabellum/src/main/java/com/parabellum/springboot/web/app/models/entity/Pelicula.java
@@ -4,9 +4,11 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -17,6 +19,7 @@ public class Pelicula implements Serializable{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@OneToMany(mappedBy="peliculas",fetch= FetchType.EAGER)
 	@Column(name = "id_pelicula")
 	private Long idPelicula;
 	
