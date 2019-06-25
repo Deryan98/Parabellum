4428fa75690d0a6b74fad5aaf72386296843a365
Cmagana6 <00061016@uca.edu.sv>
2019-06-24 22:37:45 -0600

    Proyeccion y Funcion Entity

---
+++ parabellum/src/main/java/com/parabellum/springboot/web/app/controllers/LoginController.java
@@ -0,0 +1,25 @@
+package com.parabellum.springboot.web.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.parabellum.springboot.web.app.models.service.IUsuarioService;
+
+@Controller
+public class LoginController {
+
+	@Autowired
+	IUsuarioService userService;
+	
+	@RequestMapping("/")
+	public ModelAndView login(){
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("login");
+		return mav;
+	}
+	
+	
+	
+}
