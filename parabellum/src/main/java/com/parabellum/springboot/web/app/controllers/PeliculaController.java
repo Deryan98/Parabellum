95b8306384d5262714bbaaa1c402985261732931
cjburgos21 <37281082+cjburgos21@users.noreply.github.com>
2019-06-26 00:27:53 -0600

    Agregando metodo para guardar formulario de peliculas(arreglar)

--- parabellum/src/main/java/com/parabellum/springboot/web/app/controllers/PeliculaController.java
+++ parabellum/src/main/java/com/parabellum/springboot/web/app/controllers/PeliculaController.java
@@ -2,11 +2,16 @@ package com.parabellum.springboot.web.app.controllers;
 
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.parabellum.springboot.web.app.models.entity.Pelicula;
 import com.parabellum.springboot.web.app.models.service.IPeliculaService;
@@ -33,5 +38,18 @@ public class PeliculaController {
 		model.addAttribute("peliculas", peliculaService.findAll());
 		return "tables/movie-table";
 	}
+	
+	@RequestMapping(value="/movie-form", method=RequestMethod.POST)
+	public String guardar(@Valid Pelicula pelicula, BindingResult result, Model model, SessionStatus status) {
+		if(result.hasErrors()) {
+			model.addAttribute("titulo", "Formulario de Pelicula");
+			return  "forms/movie-form";
+			
+		}
+		peliculaService.save(pelicula);
+		status.setComplete();
+		return "redirect:/admin/peliculas";
+			
+		}
 	
 }
